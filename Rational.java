2589a7c291f5f678b382bca8f56899d8f77f76b1
maxbert <mbertfield@stuy.edu>
2015-11-17 11:33:07 -0500

    i created constructors

--- Rational.java
+++ Rational.java
@@ -1,2 +1,19 @@
 public class Rational(){
+    private int numerator;
+    private int denominator;
+    public Rational(){
+	numerator = 0;
+	denominator=1;
+    }
+    public Rational(int p, int q){
+	numerator = p;
+	if (q == 0){
+	    System.out.println("you idiot, you can divide by 0!");
+	    this();
+	}else {
+	    denominator = q;
+	}
+    }
+
+
 }
