f1f8b7bd02f941836c68141a2550d06a9a45e4c1
Matthew So <mso@stuy.edu>
2015-11-17 20:30:28 -0500

    Thiscontains all the commands. Still needs comments

--- Rational.java
+++ Rational.java
@@ -1,4 +1,4 @@
-public class Rational(){
+public class Rational{
     private int numerator;
     private int denominator;
     public Rational(){
@@ -9,11 +9,25 @@ public class Rational(){
 	numerator = p;
 	if (q == 0){
 	    System.out.println("you idiot, you can divide by 0!");
-	    this();
+	    numerator = 0;
+	    denominator=1;
 	}else {
 	    denominator = q;
 	}
     }
+    public String toString(){
+	return "" + (1.0 * numerator/denominator);}
+
+    public double floatValue(){
+	return (((double) numerator)/denominator);}
+
+    public void multiply(Rational r){
+	this.numerator = r.numerator * this.numerator;
+	this.denominator = r.denominator * this.denominator;}
+
+    public void divide(Rational r){
+	this.numerator = r.denominator * this.numerator;
+	this.denominator = r.numerator * this.denominator;}
 
 
 }
