c40376a2d23809693064d7a4cda7c70ae19f86f8
seabasschin <sdittgen@stuy.edu>
2015-11-18 15:46:16 -0500

    Phase II Complete

--- Rational.java
+++ Rational.java
@@ -33,6 +33,38 @@ public class Rational{
     public void divide(Rational r){
 	this.numerator = r.denominator * this.numerator;
 	this.denominator = r.numerator * this.denominator;}
+    //add your fraction to a new one
+    public void add(Rational r){
+	this.numerator = this.numerator *  r.denominator + this.denominator * r.numerator;
+	this.denominator *= r.denominator;
+    }
+    //subtract a new fraction from your old one
+    public void subtract(Rational r){
+        this.numerator = this.numerator *  r.denominator - this.denominator * r.numerator;
+        this.denominator *= r.denominator;
+    }
+    //gcd - reused old code, uses Euclid's algorithm
+    public static int gcd(Rational r){
+        // switch inputs if b > a, because a must be >= b
+        int a = r.numerator;
+	int b = r.denominator;
 
+	if (b > a){
+            return gcd(b, a);
+        } else {
+            while((a % b) != 0){
+                int holder = a; // hold the value of a before it is replaced with b
+                a = b;
+                b = (holder % b); // b is the original remainder
+            }
+	    return b;
+	}
+    }
+    //reduce - uses gcd to reduce fraction
+    public static void reduce(Rational r){
+	int gcd = gcd(Rational r);
+	r.denominator /= gcd;
+	r.numerator /= gcd;
+    }
 
 }// end Rational
